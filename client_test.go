de6a52c996bbf826ac172c625b15914028e7f030
joinik <1793844527@qq.com>
2024-05-09 17:25:51 +0800

    TestXDail用例

--- client_test.go
+++ client_test.go
@@ -3,6 +3,8 @@ package Grpc
 import (
 	"Grpc/service"
 	"net"
+	"os"
+	"runtime"
 	"strings"
 	"testing"
 	"time"
@@ -26,4 +28,23 @@ func TestClient_dialTimeout(t *testing.T) {
 		_, err := dialTimeout(f, "tcp", l.Addr().String(), &service.Option{ConnectTimeout: 0})
 		_assert(err == nil, "0 means no limit")
 	})
+}
+
+func TestXDial(t *testing.T) {
+	if runtime.GOOS == "linux" {
+		ch := make(chan struct{})
+		addr := "/tmp/geerpc.sock"
+		go func()  {
+			_ = os.Remove(addr)
+			l, err := net.Listen("unix", addr)
+			if err != nil {
+				t.Fatal("failed to listen unix socket")
+			}
+			ch <- struct{}{}
+			service.Accept(l)
+		}()
+		<-ch
+		_, err := XDial("unix@" + addr)
+		_assert(err == nil, "failed to connect unix socket")
+	}
 }
