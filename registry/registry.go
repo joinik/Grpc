b4908fd6ac24bb821a7cf6a9904af779ead3de9f
joinik <1793844527@qq.com>
2024-05-10 16:14:36 +0800

    🚴GrpcRegistry添加服务实例和返回服务列表的方法

--- registry/registry.go
+++ registry/registry.go
@@ -1,6 +1,7 @@
 package registry
 
 import (
+	"sort"
 	"sync"
 	"time"
 )
@@ -15,17 +16,16 @@ type GrpcRegistry struct {
 }
 
 type ServerItem struct {
-	Addr string 
+	Addr  string
 	start time.Time
-
 }
 
 const (
-	defaultPath = "/_Grpc_/registry"
+	defaultPath    = "/_Grpc_/registry"
 	defaultTimeout = time.Minute * 5
 )
 
-func New(timeout time.Duration) *GrpcRegistry  {
+func New(timeout time.Duration) *GrpcRegistry {
 	return &GrpcRegistry{
 		servers: make(map[string]*ServerItem),
 		timeout: timeout,
@@ -34,3 +34,30 @@ func New(timeout time.Duration) *GrpcRegistry  {
 
 var DefaultGrpcRegister = New(defaultTimeout)
 
+// 添加服务实例
+func (r *GrpcRegistry) putServer(addr string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	s := r.servers[addr]
+	if s == nil {
+		r.servers[addr] = &ServerItem{Addr: addr, start: time.Now()}
+	} else {
+		s.start = time.Now() //if exists, update staret itme to keep alive
+	}
+}
+
+// 返回可用的服务列表，如果存在超时的服务，则删除
+func (r *GrpcRegistry) aliveServers() []string {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	var alive []string
+	for addr, s := range r.servers {
+		if r.timeout == 0 || s.start.Add(r.timeout).After(time.Now()) {
+			alive = append(alive, addr)
+		} else {
+			delete(r.servers, addr)
+		}
+	}
+	sort.Strings(alive)
+	return alive
+}
