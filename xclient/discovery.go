5396b4c2b633ade243e1d950bb3cc8e1304478cf
joinik <1793844527@qq.com>
2024-05-10 10:53:53 +0800

    🚴实现Discovery接口

--- xclient/discovery.go
+++ xclient/discovery.go
@@ -1,6 +1,7 @@
 package xclient
 
 import (
+	"errors"
 	"math"
 	"math/rand"
 	"sync"
@@ -37,4 +38,49 @@ func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
 	}
 	d.index = d.r.Intn(math.MaxInt32 - 1)
 	return d
+}
+
+// 类型检查是否实现Discovery接口
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+// Refresh doesn't make sense for MultiServersDiscovery, so ignore it
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+// Update the servers of discovery dynamically if needed
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+// Get a servewr according to mode
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRoinSelect:
+		s := d.servers[d.index%n] //servers could be updated, so mode n to ensure safety
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+// returns all servers in discovery
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	//return a copy of d.servers
+	servers := make([]string, len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
 }
