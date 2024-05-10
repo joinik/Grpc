fab419f02fbc7eb67488672a51d1943ebfa0513f
joinik <1793844527@qq.com>
2024-05-10 16:52:07 +0800

    🚴实现对应的Update，Refresh方法

--- xclient/discovery_grpc.go
+++ xclient/discovery_grpc.go
@@ -1,25 +1,60 @@
 package xclient
 
-import "time"
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
 
 type GrpcRegistryDiscovery struct {
 	*MultiServersDiscovery
-	registry string
-	timeout time.Duration
+	registry   string
+	timeout    time.Duration
 	lastUpdate time.Time
 }
 
 const defaultUpdateTimeout = time.Second * 10
 
-func NewGrpcRegistryDiscovery(registerAddr string, timeout time.Duration)*GrpcRegistryDiscovery  {
+func NewGrpcRegistryDiscovery(registerAddr string, timeout time.Duration) *GrpcRegistryDiscovery {
 	if timeout == 0 {
 		timeout = defaultUpdateTimeout
 	}
 	d := &GrpcRegistryDiscovery{
 		MultiServersDiscovery: NewMultiServerDiscovery(make([]string, 0)),
-		registry: registerAddr,
-		timeout: timeout,
+		registry:              registerAddr,
+		timeout:               timeout,
 	}
 	return d
 }
 
+func (d *GrpcRegistryDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GrpcRegistryDiscovery) Refresh() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	if d.lastUpdate.Add(d.timeout).After(time.Now()) {
+		return nil
+	}
+	log.Println("rpc registry: refresh servers from registry", d.registry)
+	resp, err := http.Get(d.registry)
+	if err != nil {
+		log.Println("rpc registry refresh err:", err)
+		return err
+	}
+	servers := strings.Split(resp.Header.Get("X-Grpc-Server"), ",")
+	d.servers = make([]string, 0, len(servers))
+	for _, server := range servers {
+		if strings.TrimSpace(server) != "" {
+			d.servers = append(d.servers, strings.TrimSpace(server))
+		}
+	}
+	d.lastUpdate = time.Now()
+	return nil
+}
