fb39e2d85855c7299b40fa60acb08ad9e0b9962a
joinik <1793844527@qq.com>
2024-05-10 10:36:42 +0800

    🚴不需要注册中心MultiServersDiscovery

--- xclient/discovery.go
+++ xclient/discovery.go
@@ -1,5 +1,12 @@
 package xclient
 
+import (
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 type SelectMode int
 
 const (
@@ -12,4 +19,22 @@ type Discovery interface {
 	Update(servers []string) error
 	Get(mode SelectMode) (string, error)
 	GetAll() ([]string, error)
+}
+
+// MultiServersDiscovery is a discovery for multi servers without a registry center
+// user provides the server addresses explicitly instead
+type MultiServersDiscovery struct {
+	r       *rand.Rand   //generate random number
+	mu      sync.RWMutex // protect following
+	servers []string
+	index   int //record the selected position for robin algorithm
+}
+
+func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
 }
