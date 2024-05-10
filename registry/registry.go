f1f6764883807f73cc6dab72ae664defce56cc8c
joinik <1793844527@qq.com>
2024-05-10 16:35:34 +0800

    🚴Heartbeat方法，服务启动时定时向注册中心发送心跳，默认周期比注册中心设置的过期时间短1min

--- registry/registry.go
+++ registry/registry.go
@@ -93,3 +93,34 @@ func (r *GrpcRegistry) HandleHTTP(registryPath string) {
 func HandleHTTP() {
 	DefaultGrpcRegister.HandleHTTP(defaultPath)
 }
+
+// Heartbeat send a heartbeat message every once in a while
+// it's a helper function for a server to register or send heartbeat
+func Heartbeat(registry, addr string, duration time.Duration) {
+	if duration == 0 {
+		// make sure there is enough time to send heart beat
+		// before it's removed form registry
+		duration = defaultTimeout - time.Duration(1)*time.Minute
+	}
+	var err error
+	err = sendHeartbeat(registry, addr)
+	go func() {
+		t := time.NewTicker(duration)
+		for err == nil {
+			<-t.C
+			err = sendHeartbeat(registry, addr)
+		}
+	}()
+}
+
+func sendHeartbeat(registry, addr string) error {
+	log.Println(addr, "send heart beat to registry", registry)
+	httpClient := &http.Client{}
+	req, _ := http.NewRequest("POST", registry, nil)
+	req.Header.Set("X-Grpc-Server", addr)
+	if _, err := httpClient.Do(req); err != nil {
+		log.Println("rpc server: heart beat err:", err)
+		return err
+	}
+	return nil
+}
